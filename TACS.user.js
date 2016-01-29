94faf127b572b8335b808d99c67a3ef98c7132ff
KRS-L <KRS-L@users.noreply.github.com>
2016-01-29 19:58:30 +1000

    layout fixes

--- TACS.user.js
+++ TACS.user.js
@@ -3,7 +3,7 @@
 // @description    Allows you to simulate combat before actually attacking.
 // @namespace      https://prodgame*.alliances.commandandconquer.com/*/index.aspx*
 // @include        https://prodgame*.alliances.commandandconquer.com/*/index.aspx*
-// @version        3.27b
+// @version        3.28b
 // @author         KRS_L | Contributions/Updates by WildKatana, CodeEcho, PythEch, Matthias Fuchs, Enceladus, TheLuminary, Panavia2, Da Xue, MrHIDEn, TheStriker, JDuarteDJ, null
 // @translator     TR: PythEch | DE: Matthias Fuchs, Leafy & sebb912 | PT: JDuarteDJ & Contosbarbudos | IT: Hellcco | NL: SkeeterPan | HU: Mancika | FR: Pyroa & NgXAlex | FI: jipx | RO: MoshicVargur
 // @grant none
@@ -528,7 +528,7 @@ window.TACS_version = GM_info.script.version;
 							if (temp) {
 								this._armyBar.add(this.buttons.attack.unlock, {
 									top : 108,
-									right : 9
+									right : 10
 								});
 							}
 
@@ -551,8 +551,8 @@ window.TACS_version = GM_info.script.version;
 							}
 							if (temp) {
 								this._armyBar.add(this.buttons.attack.repair, {
-									top : 16,
-									right : 9
+									top : 23,
+									right : 10
 								});
 							}
 
