e8e22308321ac72c159c980a267d37950dc2c6d8
KRS-L <KRS-L@users.noreply.github.com>
2018-07-12 02:48:04 +1000

    compatibility update for server move

--- URL_Bug_Fix.user.js
+++ URL_Bug_Fix.user.js
@@ -2,10 +2,8 @@
 // @name Tiberium Alliances URL Bug Fix
 // @description Simple solution to the ancient URL bug that EA has been unable or unwilling to fix.
 // @namespace URL_Bug_Fix
-// @include https://prodgame*.alliances.commandandconquer.com/*/index.aspx*
-// @version 1.1
-// @updateURL https://userscripts.org/scripts/source/204128.meta.js
-// @downloadURL https://userscripts.org/scripts/source/204128.user.js
+// @include https://*.alliances.commandandconquer.com/*/index.aspx*
+// @version 1.1.1
 // ==/UserScript==
 (function () {
 	var URL_Bug_Fix_main = function () {
