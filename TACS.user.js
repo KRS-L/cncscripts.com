f94ddc12a07e85c8eb494831bbb0c97094888997
KRS-L <KRS-L@users.noreply.github.com>
2026-08-12 13:45:15 +1000

    adjust repair button lock position

--- TACS.user.js
+++ TACS.user.js
@@ -3,7 +3,7 @@
 // @description    Allows you to simulate combat before actually attacking.
 // @namespace      https://*.alliances.commandandconquer.com/*/index.aspx*
 // @include        https://*.alliances.commandandconquer.com/*/index.aspx*
-// @version        3.53b.2026.4
+// @version        3.53b.2026.5
 // @author         KRS_L | Contributions/Updates by WildKatana, CodeEcho, PythEch, Matthias Fuchs, Enceladus, TheLuminary, Panavia2, Da Xue, MrHIDEn, TheStriker, JDuarteDJ, null, g3gg0.de
 // @translator     TR: PythEch | DE: Matthias Fuchs, Leafy & sebb912 | PT: JDuarteDJ & Contosbarbudos | IT: Hellcco | NL: SkeeterPan | HU: Mancika | FR: Pyroa & NgXAlex | FI: jipx | RO: MoshicVargur
 // @grant none
@@ -560,7 +560,7 @@ window.TACS_version = GM_info.script.version;
 							}
 							if (temp) {
 								this._armyBar.add(this.buttons.attack.repair, {
-									top: 23,
+									top: 63,
 									right: 10
 								});
 							}
