43bfcd959d700fa4a413e0e40d406f0cbda49215
KRS-L <KRS-L@users.noreply.github.com>
2026-08-23 23:08:05 +1000

    fix empty battleground in simulation view

--- TACS.user.js
+++ TACS.user.js
@@ -3,7 +3,7 @@
 // @description    Allows you to simulate combat before actually attacking.
 // @namespace      https://*.alliances.commandandconquer.com/*/index.aspx*
 // @include        https://*.alliances.commandandconquer.com/*/index.aspx*
-// @version        3.53b.2026.6
+// @version        3.53b.2026.7
 // @author         KRS_L | Contributions/Updates by WildKatana, CodeEcho, PythEch, Matthias Fuchs, Enceladus, TheLuminary, Panavia2, Da Xue, MrHIDEn, TheStriker, JDuarteDJ, null, g3gg0.de
 // @translator     TR: PythEch | DE: Matthias Fuchs, Leafy & sebb912 | PT: JDuarteDJ & Contosbarbudos | IT: Hellcco | NL: SkeeterPan | HU: Mancika | FR: Pyroa & NgXAlex | FI: jipx | RO: MoshicVargur
 // @grant none
@@ -3009,6 +3009,7 @@ window.TACS_version = GM_info.script.version;
 							if (!this.statsOnly) {
 								this.enterSimulationView();
 								setTimeout(function () {
+									ClientLib.Vis.VisMain.GetInstance().get_Battleground().RestartReplay();
 									ClientLib.Vis.VisMain.GetInstance().get_Battleground().set_ReplaySpeed(1);
 								}, 1);
 							}
